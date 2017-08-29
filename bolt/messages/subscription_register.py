34a302a850382d2e37163e3cca7f0f70994d2032
Saurabh Badhwar <sbadhwar@redhat.com>
2017-08-29 15:29:27 +0530

    Provide an implementation for the subscription manager registration
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- bolt/messages/subscription_register.py
+++ bolt/messages/subscription_register.py
@@ -17,9 +17,56 @@ class SubscriptionRegister(MessageBase):
         """SubscriptionRegister constructor
 
         Initializes the message for subscription registration
+
+        Keyword arguments:
+            username(str): The username to use for registration
+            password(str): The password to use for registration
+            environment(str): The name of the environment to attach to
+            activation_key(str): The activation key to be used
         """
 
         super(SubscriptionRegister, self).__init__(self.__class__.__name__)
         self.set_command('subscription-manager')
         self.set_subcommand('register')
         self.set_extras('option_renderer:=')
+
+    def __str__(self):
+        """Provide a string based representation of the class"""
+
+        return "<subscription-manager register>"
+
+    def set_username(self, username):
+        """Set the username for the command
+
+        Args:
+            username(str): The username to be set
+        """
+
+        self.set_options('username', username)
+
+    def set_password(self, password):
+        """Set the password for the command
+
+        Args:
+            password(str): The password to be set
+        """
+
+        self.set_options('password', password)
+
+    def set_environment(self, environment):
+        """Set the environment for the command
+
+        Args:
+            environment(str): The environment to be set
+        """
+
+        self.set_options('environment', environment)
+
+    def set_actkey(self, act_key):
+        """Set the activation key to be used
+
+        Args:
+            act_key(str): The activation key to be used
+        """
+
+        self.set_options('activationkey', act_key)
