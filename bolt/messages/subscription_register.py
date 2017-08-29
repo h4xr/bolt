0130c21e54ab738761d6bd841c25ebe22fba9aed
Saurabh Badhwar <sbadhwar@redhat.com>
2017-08-29 15:09:05 +0530

    Basic implementation of subscription-register message
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

---
+++ bolt/messages/subscription_register.py
@@ -0,0 +1,25 @@
+'''
+Name: subscription_register.py
+Description: Subscription manager registration message
+Author: Saurabh Badhwar <sbadhwar@redhat.com>
+Date: 29/08/2017
+'''
+from base import MessageBase
+
+class SubscriptionRegister(MessageBase):
+    '''Message implementation for calling subscription manager register
+
+    Implements the variable type of subscription manager registration messages
+    based on the provided arguments.
+    '''
+
+    def __init__(self, **kwargs):
+        """SubscriptionRegister constructor
+
+        Initializes the message for subscription registration
+        """
+
+        super(SubscriptionRegister, self).__init__(self.__class__.__name__)
+        self.set_command('subscription-manager')
+        self.set_subcommand('register')
+        self.set_extras('option_renderer:=')
